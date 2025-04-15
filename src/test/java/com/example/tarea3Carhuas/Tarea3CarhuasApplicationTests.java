3adfe3aaefb6f2c4786a657bc7ecc221d2fd670b
Angie Carhuas <142537034+Angierc25@users.noreply.github.com>
2025-04-15 17:03:28 -0500

    Add files via upload

---
+++ src/test/java/com/example/tarea3Carhuas/Tarea3CarhuasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarea3Carhuas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea3CarhuasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
